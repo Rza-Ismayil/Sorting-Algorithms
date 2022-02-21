6d3c676d53df182ea56d121efd21ef1b60bc131f
Rza Ismayilov <rzaismail82@gmail.com>
2022-02-21 22:13:14 +0400

    small fix

--- src/selection.cpp
+++ src/selection.cpp
@@ -9,22 +9,22 @@ void swap(int *a, int x, int y)
 
 int locationOfSmallest(int *array, int start, int end)
 {
-    int j = start;
-    for (int i = start; i <= end; i++)
+    int minIndex = start;
+    for (int i = start; i < end; i++)
     {
-        if (array[i] < array[j])
+        if (array[i] < array[minIndex])
         {
-            j = i;
+            minIndex = i;
         }
     }
-    return j;
+    return minIndex;
 }
 
 void selectionSort(int *array, int size)
 {
     for (int i = 0; i < size - 1; i++)
     {
-        int j = locationOfSmallest(array, i, size - 1);
+        int j = locationOfSmallest(array, i, size);
         swap(array, i, j);
     }
 }
